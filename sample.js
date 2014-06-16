93918c18de502453d48d634096247041aa5c817e
Stepan Karslyan <stepan.karslyan@gmail.com>
2014-06-16 15:56:21 +0400

    Killer is ok with ps_vision

--- sample.js
+++
@@ -1,8 +0,0 @@
-var zmq = require("zmq");
-var requester = zmq.socket("asyncreq");
-requester.connect("tcp://localhost:4040");
-  console.log("Connecting to the 4040");
-requester.send(JSON.stringify({pid: "7152"}),function(response) {
-  console.log(response);
-});
-
--- server.js
+++ server.js
@@ -2,17 +2,21 @@ var exec = require("child_process").exec;
 var zmq = require("zmq");
 
 var responder = zmq.socket("asyncrep");
-responder.bind("tcp://*:4040");
-console.log("Binding to: tcp://*:4040");
+responder.bind("tcp://*:4141");
+console.log("Binding to: tcp://*:4141");
 
 responder.on("message", function(message, response) {
   var request = JSON.parse(message);
-  console.log("Requester sends: " + JSON.stringify(request));
-  exec("kill -9 " + request.pid, function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
+  console.log("Requester sends: " + request.toString());
+  
+  if(request.type == "delete") {
+  
+    exec("kill -9 " + request.pid, function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
     
-  });
+    });
+  }
     
   console.log("Process with" + request.pid + " " + "was killed");
   response.send("Process with" + request.pid + " " + "was killed");
